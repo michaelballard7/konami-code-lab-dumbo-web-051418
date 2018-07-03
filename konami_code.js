3a15e6a14d6f68dcb246e0589d725c29e558e2db
Michael Ballard <ballard.michael9@gmail.com>
2018-07-03 12:53:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,5 +13,17 @@ const codes = [
 
 function init() {
   // your code here
-  
+   let abc = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    abc = (codes[abc] === key) ? ++abc : 0
+    
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+    
+  });
 }
