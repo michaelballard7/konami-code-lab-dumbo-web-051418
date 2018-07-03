247c29a8a3e8a107c2d64c9d3ab4b10740781aab
Michael Ballard <ballard.michael9@gmail.com>
2018-07-03 12:54:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,17 @@ const codes = [
 
 function init() {
   // your code here
+   let abc = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    abc = (codes[abc] === key) ? ++abc : 0
+    
+    if (abc === codes.length) {
+      window.alert("Hurray!");
+      abc = 0
+    }
+    
+  });
 }
